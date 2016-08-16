20e9459c912e959de7a3a057f49e4c50245258f5
guillotz <guillermo_fink@hotmail.com>
2016-08-16 20:24:53 -0300

    Subo index php

---
+++ Index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Fink</title>
+</head>
+<body>
+<h1>Hola Mundo</h1>
+<?php
+echo "Hola PHP";
+echo "<h2>Hola PHP2</h2>";
+$numero = 10;
+echo "<br>El numero es: ".$numero;
+echo "<br>El numero es: $numero";
+echo '<br>El numero es: $numero';
+?>
+</body>
+</html>
