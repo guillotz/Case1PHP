95aefaeab902fa417a91bd7b8d01f080219ae6fd
guillotz <guillermo_fink@hotmail.com>
2016-08-16 21:08:23 -0300

    Genero random

--- Index.php
+++ Index.php
@@ -5,13 +5,17 @@
 <body>
 <h1>Hola Mundo</h1>
 <?php
-echo "Hola PHP";
+/*echo "Hola PHP";
 echo "<h2>Hola PHP2</h2>";
 $numero = 10;
 echo "<br>El numero es: ".$numero;
 echo "<br>El numero es: $numero";
 echo '<br>El numero es: $numero';
 echo "El cambio";
+*/
+//Crear un numero random y mostrarlo
+$random = (rand(0,10));
+echo "Numero: $random";
 ?>
 </body>
 </html>
